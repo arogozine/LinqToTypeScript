17643e0aabdb9953d88e76750ce58ae26c039acc
arogozine <alexandre.rogozine@live.com>
2017-10-27 17:45:47 -0600

    Remove outdates lib files

--- lib/LinqForMap.d.ts
+++
@@ -1,5 +0,0 @@
-import { IEnumerable } from "./Interfaces";
-declare global  {
-    interface Map<K, V> extends IEnumerable<[K, V]> {
-    }
-}
--- lib/LinqForSet.d.ts
+++
@@ -1,5 +0,0 @@
-import { IEnumerable } from "./Interfaces";
-declare global  {
-    interface Set<T> extends IEnumerable<T> {
-    }
-}
