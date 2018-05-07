b8eed9526fff5f9396c0484dc6657636456637c9
arogozine <alexandre.rogozine@live.com>
2018-05-07 17:58:16 -0600

    Fix MaxAsync functions

--- src/sync/Enumerable.ts
+++ src/sync/Enumerable.ts
@@ -1590,7 +1590,7 @@ export class Enumerable {
         source: Iterable<TSource>, selector: (x: TSource) => Promise<number>): Promise<number> {
         let max: number | null = null
         for (const item of source) {
-            max = Math.max(max || Number.MIN_VALUE, await selector(item))
+            max = Math.max(max || Number.NEGATIVE_INFINITY, await selector(item))
         }
 
         if (max === null) {
