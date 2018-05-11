ba139ffe020fbc7547b80251fa82932bf806bc61
arogozine <alexandre.rogozine@live.com>
2018-05-10 19:14:25 -0600

    Add CountAsync tests

--- test/tests/Count.ts
+++ test/tests/Count.ts
@@ -1,4 +1,4 @@
-import { asAsync, expectAsync, itAsync, itEnumerable } from "./../TestHelpers"
+import { asAsync, expectAsync, itAsync, itEnumerable, itParallel } from "./../TestHelpers"
 
 describe("count", () => {
     itEnumerable<boolean>("Count Predicate", (asEnumerable) => {
@@ -8,8 +8,15 @@ describe("count", () => {
         expect(array.count((x) => !x)).toBe(1)
     })
 
-    itAsync("CountPredicateAsync", async () => {
+    itAsync("Count Predicate Async", async () => {
         const array = asAsync([true, true, false])
+
+        expect(await array.count((x) => x)).toBe(2)
+        expect(await array.count((x) => !x)).toBe(1)
+    })
+
+    itParallel<boolean>("Count Predicate Async", async (asParallel) => {
+        const array = asParallel([true, true, false])
 
         expect(await array.count((x) => x)).toBe(2)
         expect(await array.count((x) => !x)).toBe(1)
@@ -21,9 +28,17 @@ describe("count", () => {
     itAsync("empty array to be zero async", async () =>
         (await expectAsync(asAsync([]).count())).toBe(0))
 
+    itParallel("empty array to be zero async", async (asParallel) => {
+        const expect = await expectAsync(asParallel([]).count())
+        expect.toBe(0)
+    })
+
     itEnumerable("single element array to be one", (asEnumerable) =>
         expect(asEnumerable([1]).count()).toBe(1))
 
     itAsync("single element array to be one Async", async () =>
         expect(await asAsync([1]).count()).toBe(1))
+
+    itParallel("single element array to be one Async", async (asParallel) =>
+        expect(await asParallel([1]).count()).toBe(1))
 })
