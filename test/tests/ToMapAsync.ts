b96b5a06843a3aa9f78ceb71f25626a3f1e94fd3
arogozine <alexandre.rogozine@live.com>
2018-05-10 19:27:28 -0600

    Add ToMapAsync tests

---
+++ test/tests/ToMapAsync.ts
@@ -0,0 +1,30 @@
+import { asAsync, itAsync, itEnumerableAsync, itParallel, asPromise } from "../TestHelpers"
+
+describe("toMapAsync", () => {
+    itEnumerableAsync("toMap", async (asEnumerable) => {
+        const map = await asEnumerable([1, 2, 3]).toMapAsync((x) => asPromise(`Key_${ x }`))
+        for (const keyValue of map) {
+            const key = keyValue[0]
+            const value = keyValue[1]
+            expect(key).toBe(`Key_${ value[0] }`)
+        }
+    })
+
+    itAsync("toMapAsync", async () => {
+        const map = await asAsync([1, 2, 3]).toMapAsync((x) => asPromise(`Key_${ x }`))
+        for (const keyValue of map) {
+            const key = keyValue[0]
+            const value = keyValue[1]
+            expect(key).toBe(`Key_${ value[0] }`)
+        }
+    })
+
+    itParallel("toMapParallel", async (asParallel) => {
+        const map = await asParallel([1, 2, 3]).toMapAsync((x) => asPromise(`Key_${ x }`))
+        for (const keyValue of map) {
+            const key = keyValue[0]
+            const value = keyValue[1]
+            expect(key).toBe(`Key_${ value[0] }`)
+        }
+    })
+})
