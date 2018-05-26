bbfdc12383aa55dfbb919a56c8df7d13134c5bce
arogozine <alexandre.rogozine@live.com>
2018-05-26 16:49:50 -0600

    Add sequence equals tests

---
+++ test/tests/SequenceEquals.ts
@@ -0,0 +1,40 @@
+import { EqualityComparer } from "../../src"
+import { asAsync, itAsync, itEnumerable, itParallel } from "../TestHelpers"
+
+describe("SequenceEquals", () => {
+    itEnumerable("Sequence Equals Basic", (asEnumerable) => {
+        const sequenceEquals = asEnumerable([1, 2, 3, 4, 5])
+            .sequenceEquals(asEnumerable([1, 2, 3, 4, 5]))
+        expect(sequenceEquals).toBe(true)
+    })
+
+    itEnumerable<string | number>("Sequence Equals Weak Comparer Basic", (asEnumerable) => {
+        const sequenceEquals = asEnumerable([1, "2", 3, 4, "5"])
+            .sequenceEquals(asEnumerable([1, 2, 3, "4", 5]), EqualityComparer)
+        expect(sequenceEquals).toBe(true)
+    })
+
+    itAsync("Sequence Equals Async Basic", async () => {
+        const sequenceEquals = await asAsync([1, 2, 3, 4, 5])
+            .sequenceEquals(asAsync([1, 2, 3, 4, 5]))
+        expect(sequenceEquals).toBe(true)
+    })
+
+    itAsync("Sequence Equals Async Weak Comparer Basic", async () => {
+        const sequenceEquals = await asAsync(["1", "2", 3, 4, 5])
+            .sequenceEquals(asAsync([1, 2, 3, 4, "5"]), EqualityComparer)
+        expect(sequenceEquals).toBe(true)
+    })
+
+    itParallel("Sequence Equals Parallel Basic", async (asParallel) => {
+        const sequenceEquals = await asParallel([1, 2, 3, 4, 5])
+            .sequenceEquals(asParallel([1, 2, 3, 4, 5]))
+        expect(sequenceEquals).toBe(true)
+    })
+
+    itParallel<string | number>("Sequence Equals Parallel Weak Parallel Basic", async (asParallel) => {
+        const sequenceEquals = await asParallel([1, 2, 3, "4", "5"])
+            .sequenceEquals(asParallel(["1", "2", "3", 4, 5]), EqualityComparer)
+        expect(sequenceEquals).toBe(true)
+    })
+})
---
+++ test/tests/SequenceEqualsAsync.ts
@@ -0,0 +1,26 @@
+import { asAsync, itAsync, itEnumerableAsync, itParallel } from "../TestHelpers"
+
+// tslint:disable-next-line:triple-equals
+const AsyncEqualityComparer = async <T>(x: T, y: T) => x == y
+
+describe("sequenceEqualsAsync", () => {
+    itEnumerableAsync<string | number>("Sequence Equals Async Basic", async (asEnumerable) => {
+        const sequenceEquals = await asEnumerable([1, "2", 3, 4, "5"])
+            .sequenceEqualsAsync(asEnumerable([1, 2, 3, "4", 5]), AsyncEqualityComparer)
+        expect(sequenceEquals).toBe(true)
+    })
+
+    /*
+    itAsync("Sequence Equals Async Weak Comparer Basic", async () => {
+        const sequenceEquals = await asAsync(["1", "2", 3, 4, 5])
+            .sequenceEqualsAsync(asAsync([1, 2, 3, 4, "5"]), AsyncEqualityComparer)
+        expect(sequenceEquals).toBe(true)
+    })
+    */
+
+    itParallel<string | number>("Sequence Equals Parallel Basic", async (asParallel) => {
+        const sequenceEquals = await asParallel([1, 2, 3, "4", "5"])
+            .sequenceEqualsAsync(asParallel(["1", "2", "3", 4, 5]), AsyncEqualityComparer)
+        expect(sequenceEquals).toBe(true)
+    })
+})
