2dc84332c1451adf9a7b130e1873b6dbf7a95aee
arogozine <alexandre.rogozine@live.com>
2018-06-12 07:21:49 -0600

    Make Grouping implement IGrouping interface explicitly

--- lib/sync/Grouping.d.ts
+++ lib/sync/Grouping.d.ts
@@ -1,8 +1,9 @@
+import { IGrouping } from "../shared/shared";
 import { ArrayEnumerable } from "./ArrayEnumerable";
 /**
  * Key to Values Enumeration
  */
-export declare class Grouping<TKey, TElement> extends ArrayEnumerable<TElement> {
+export declare class Grouping<TKey, TElement> extends ArrayEnumerable<TElement> implements IGrouping<TKey, TElement> {
     readonly key: TKey;
     constructor(key: TKey, startingItem: TElement);
 }
--- src/sync/Grouping.ts
+++ src/sync/Grouping.ts
@@ -1,9 +1,10 @@
+import { IGrouping } from "../shared/shared"
 import { ArrayEnumerable } from "./ArrayEnumerable"
 
 /**
  * Key to Values Enumeration
  */
-export class Grouping<TKey, TElement> extends ArrayEnumerable<TElement> {
+export class Grouping<TKey, TElement> extends ArrayEnumerable<TElement> implements IGrouping<TKey, TElement> {
     public readonly key: TKey
     constructor(key: TKey, startingItem: TElement) {
         super(1)
