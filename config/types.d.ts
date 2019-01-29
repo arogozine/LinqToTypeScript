a3c3bf418e0a7b65ccd9082b650cc58ce1d299fa
arogozine <alexandre.rogozine@live.com>
2019-01-28 19:41:33 -0700

    Configuration Changes WIP

---
+++ config/types.d.ts
@@ -0,0 +1,17 @@
+// https://raw.githubusercontent.com/Hotell/typescript-lib-starter/master/config/types.d.ts
+// https://github.com/Hotell/typescript-lib-starter/blob/master/LICENSE.md
+
+// ===== JEST ====
+export type TsJestConfig = import('ts-jest/dist/types').TsJestConfig
+export type JestConfig = Partial<jest.ProjectConfig & jest.GlobalConfig>
+
+// ==== PRETTIER ====
+// export type PrettierConfig = import('prettier').Options
+
+// ==== ROLLUP ====
+export type RollupConfig = import('rollup').InputOptions & {
+  output:
+    | import('rollup').OutputOptions
+    | Array<import('rollup').OutputOptions | null>
+}
+export type RollupPlugin = import('rollup').Plugin
