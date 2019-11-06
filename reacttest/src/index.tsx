de116001139c51e9f5bf42043429e8139bd34cc2
arogozine <alexandre.rogozine@live.com>
2019-11-06 14:30:10 -0700

    Add tests for react

---
+++ reacttest/src/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+// 0. Import Module
+import { initializeLinq, IEnumerable, ArrayEnumerable } from "linq-to-typescript"
+// 1. Declare that the JS types implement the IEnumerable interface
+declare global {
+    interface Array<T> extends IEnumerable<T> {
+        concat(items: IEnumerable<T>): IEnumerable<T>;
+        concat(...items: Array<ReadonlyArray<T>>): ArrayEnumerable<T>;
+        concat(...items: Array<T | ReadonlyArray<T>>): ArrayEnumerable<T>;    
+    }
+    interface Uint8Array extends IEnumerable<number> { }
+    interface Uint8ClampedArray extends IEnumerable<number> { }
+    interface Uint16Array extends IEnumerable<number> { }
+    interface Uint32Array extends IEnumerable<number> { }
+    interface Int8Array extends IEnumerable<number> { }
+    interface Int16Array extends IEnumerable<number> { }
+    interface Int32Array extends IEnumerable<number> { }
+    interface Float32Array extends IEnumerable<number> { }
+    interface Float64Array extends IEnumerable<number> { }
+    interface Map<K, V> extends IEnumerable<[K, V]> { }
+    interface Set<T> extends IEnumerable<T> { }
+    interface String extends IEnumerable<string> { }
+}
+// 2. Bind Linq Functions to Array and Map
+initializeLinq()
+// 3. Use without a wrapper type
+const evenNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9].where((x) => x % 2 === 0).toArray()
+console.log(evenNumbers);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
