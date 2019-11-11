caadf4f3840bf24b3c5c83dc34f570058ef06457
arogozine <alexandre.rogozine@live.com>
2019-11-11 16:57:31 -0700

    Test async iterable with node test

--- nodetest/index.ts
+++ nodetest/index.ts
@@ -7,4 +7,11 @@ const primeNumbers = range(2, 10000)
     .select(([prime]) => prime)
     .toArray()
 
+async function asyncIterable() {
+    for await (const i of  range(0, 10).selectAsync(async (x) => x * 2)) {
+        console.log(i)
+    }
+}
+
+asyncIterable()
 console.log(primeNumbers)
