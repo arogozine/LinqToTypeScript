359d3e2c8d8da6b0d3899a91feafaf4d6c3dfef4
arogozine <alexandre.rogozine@live.com>
2020-11-23 17:23:53 -0700

    Don't override message in InvalidOperation Ex

--- src/shared/InvalidOperationException.ts
+++ src/shared/InvalidOperationException.ts
@@ -2,7 +2,7 @@
  * Invalid Operation Exception
  */
 export class InvalidOperationException extends Error {
-    public constructor(public readonly message: string) {
+    public constructor(message: string) {
         super(message)
         this.name = `InvalidOperationException`
         this.stack = this.stack || (new Error()).stack
