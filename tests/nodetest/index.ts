c8d9efee530d04cbd9b33a2e9fadcb52709364cb
Alexandre Rogozine <alexandre.rogozine@live.com>
2022-08-14 12:34:44 -0600

    Fix Node Test

--- tests/nodetest/index.ts
+++ tests/nodetest/index.ts
@@ -1,17 +1,19 @@
-import { range } from "linq-to-typescript"
+const promise = import("linq-to-typescript")
 
-const primeNumbers = range(2, 10000)
+promise.then(({range}) => {
+    const primeNumbers = range(2, 10000)
     .select((i) => [i, Math.floor(Math.sqrt(i))])
     .where(([i, iSq]) =>
         range(2, iSq).all((j) => i % j !== 0))
     .select(([prime]) => prime)
     .toArray()
 
-async function asyncIterable() {
-    for await (const i of range(0, 10).selectAsync(async (x) => x * 2)) {
-        console.log(i)
+    async function asyncIterable() {
+        for await (const i of range(0, 10).selectAsync(async (x) => x * 2)) {
+            console.log(i)
+        }
     }
-}
 
-asyncIterable()
-console.log(primeNumbers)
+    asyncIterable()
+    console.log(primeNumbers)
+})
