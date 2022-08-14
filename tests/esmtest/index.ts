18913e7ab055a9b2e8bbd14dae32084ec85a841b
Alexandre Rogozine <alexandre.rogozine@live.com>
2022-08-14 09:54:24 -0600

    Add ESM module tests

---
+++ tests/esmtest/index.ts
@@ -0,0 +1,17 @@
+import { range } from "linq-to-typescript"
+
+const primeNumbers = range(2, 10000)
+    .select((i) => [i, Math.floor(Math.sqrt(i))])
+    .where(([i, iSq]) =>
+        range(2, iSq).all((j) => i % j !== 0))
+    .select(([prime]) => prime)
+    .toArray()
+
+async function asyncIterable() {
+    for await (const i of range(0, 10).selectAsync(async (x) => x * 2)) {
+        console.log(i)
+    }
+}
+
+asyncIterable()
+console.log(primeNumbers)
